74765168732772288ebdb19fcb1487003ee8bae0
minhajul sarkar <minhajul.sarkar@gmail.com>
2018-12-25 21:18:38 +0600

    File write from base64 encode string

--- src/test/java/com/filestorage/stub/inventorystub/FileStorageServiceTest.java
+++ src/test/java/com/filestorage/stub/inventorystub/FileStorageServiceTest.java
@@ -4,6 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -23,6 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FileStorageServiceTest {
 	private static final String END_POINT = "http://localhost:8080";
+	private static final String PATH = "/tmp";
 	private String fileName = "263ed79a065d4274b041d9e5cc64f99d.png";
 	
 	FileStorageServiceAPI api;
@@ -33,7 +40,7 @@ public class FileStorageServiceTest {
 
 	@Test
 	public void test1Upload() throws JsonProcessingException {
-		File file = new File("/tmp/dashboard.png");
+		File file = new File(PATH + File.separator + "dashboard.png");
 		FileResponse fileResponse = FileStorageServiceAPI.connect(END_POINT).upload(file);
 		assertNotNull(fileResponse);
 		assertEquals("success", fileResponse.getStatus());
@@ -41,10 +48,13 @@ public class FileStorageServiceTest {
 	}
 	
 	@Test
-	public void test2Download() throws JsonProcessingException {
+	public void test2Download() throws IOException {
 		FileResponse response = FileStorageServiceAPI.connect(END_POINT).download(fileName);
 		assertNotNull(response);
 		assertEquals("success", response.getStatus());
 		log.info(response.getFile());
+		byte[] decodedImg = Base64.getDecoder().decode(response.getFile().getBytes(StandardCharsets.UTF_8));
+		Path destinationFile = Paths.get(PATH, fileName);
+		Files.write(destinationFile, decodedImg);
 	}
 }
